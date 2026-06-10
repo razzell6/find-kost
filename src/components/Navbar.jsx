a30541236c4bf3aacf986bbbb739a708e51cd631
razzell6 <hafidzraharjo123@gmail.com>
2026-06-10 23:49:53 +0700

    mengubah teks navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,8 +24,6 @@ export default function Navbar() {
         {/* Menu Navigasi */}
         <div className="hidden md:flex items-center gap-6 text-sm font-medium text-slate-600">
           <Link to="/" className="hover:text-indigo-600 transition">Home</Link>
-          <Link to="#" className="hover:text-indigo-600 transition">Cari Kost</Link>
-          <Link to="#" className="hover:text-indigo-600 transition">Contact</Link>
         </div>
 
         {/* Tombol Aksi Kanan */}
